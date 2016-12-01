3944c969d9da027c078c87b69ef60b6f11bd2f81
oshovskiy88 <oshovskiy88@gmail.com>
2016-12-01 16:36:40 +0200

    MAG METOD

--- www/index.php
+++ www/index.php
@@ -6,7 +6,7 @@
 //git config --global user.name "Fedor Emelianenko"
 class Room
 {
-    public $color='green';
+    public $color='green<br>';
     public function getColor()
     {
         echo $this->color;
@@ -15,10 +15,43 @@ class Room
         $this->color=$color;
     }
 }
+
 $ob= new Room();
-$ob->color='red';
-
-$ob->changeColor('blue');
+echo $ob->color;
+//$ob->changeColor('blue');
 $ob->getColor();
+class Room2
+{
+    static $color='red Blue<br>';
+    public static function getColor2()
+    {
+        echo static::$color;
+    }
+}
+Room2::getColor2();
+echo Room2::$color;
+$ob2 = new Room2;
+echo $ob2::$color;
+$ob2::getColor2();
 
+class Banya {
+    public function __construct()
+    {
+        echo "VSE BUDE OK<br>";
+    }
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+        echo "Мы обратились к обьекту как к ф-ии<br>";
 
+    }
+    public function __toString()
+    {
+        echo 'Мы обратились к обьекту как к строке<br>';
+
+    }
+}
+
+$obj= new Banya;
+$obj();
+//echo $obj;
