1097028c25693a6e12b447f7f2285aaa44d02f76
oshovskiy88 <oshovskiy88@gmail.com>
2016-12-01 12:29:48 +0200

    OOP

--- www/index.php
+++ www/index.php
@@ -1,6 +1,24 @@
 <?php
-phpinfo();
+//phpinfo();
 //побороть ошибку git
 //git config --global user.email "ge*****gmail.com"
 
 //git config --global user.name "Fedor Emelianenko"
+class Room
+{
+    public $color='green';
+    public function getColor()
+    {
+        echo $this->color;
+    }
+    public function changeColor($color){
+        $this->color=$color;
+    }
+}
+$ob= new Room();
+$ob->color='red';
+
+$ob->changeColor('blue');
+$ob->getColor();
+
+
