3bfd65f382d29ebc4a87f055e68739a2b2cf1be0
oshovskiy88 <oshovskiy88@gmail.com>
2017-03-09 22:28:08 +0200

    sql

--- app/paymentSystem/PayPal.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace app\paymentSystem;
-class PayPal implements PaymentSystemInterface
-{
-    public function paymont()
-    {
-        echo "Система PayPal выполнила платеж!!";
-    }
-}
